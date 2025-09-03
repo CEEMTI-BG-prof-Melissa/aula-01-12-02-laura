2b4803369e8db4d207494410f8c42b3b3f562f29
laurienny <laurienny.msouza@aluno.edu.es.gov.br>
2025-09-03 11:02:27 -0300

    Create Aula 02 - 3 trimestre _ index.php

---
+++ Aula 02 - 3 trimestre _ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Aula 02 PHP></title>
+    </head>
+    
+    <body>
+        <?php
+         $nome1="Laurienny";
+
+         echo "meu nome é " .nome1;
+        ?>
+    </body>
+<html>
