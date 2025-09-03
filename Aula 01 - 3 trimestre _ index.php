71350041d6c67e043e6052cdfee07405d4a64952
laurienny <laurienny.msouza@aluno.edu.es.gov.br>
2025-09-03 10:42:33 -0300

    Create Aula 01 - 3 trimestre _ index.php

---
+++ Aula 01 - 3 trimestre _ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Aula 01 PHP></title>
+    </head>
+    <body>
+        <?php
+           echo "olá, mundo";
+           echo "melissa";
+        ?>
+    </body>
+<html>
